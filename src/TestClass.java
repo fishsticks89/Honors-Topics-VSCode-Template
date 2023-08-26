aba4ee3c7980a4ef6fa71a3603501d5c472dddbe
Michael Barr <61331006+fishsticks89@users.noreply.github.com>
2023-08-26 12:49:45 -0700

    heheheha

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,10 +1,2 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-public class TestClass {
-// paste tester contents here
-}
+// paste tester contents here, and then rename 
+// the class (e.g. `MyStringTester`) to `TestClass`
